495f73327d1afb9c7acc319d1f723be4a5786ad6
rohit <01fe22bca031@kletech.ac.in>
2024-04-03 22:09:43 +0530

    employee program

---
+++ employee1.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class employee1 extends devops_eng {
+    int id;
+    String name;
+    int salary;
+    void read(){
+        System.out.println("Enter id name and salary of employee");
+        Scanner s=new Scanner(System.in);
+        id=s.nextInt();
+        name=s.next();
+        salary=s.nextInt();
+    }
+    void display(){
+        System.out.println("Id: "+id+"\nName: "+name+"\nSalary: "+salary);
+    }
+    public static void main(String[] args) {
+        employee1 e1=new employee1();
+        devops_eng d1=new devops_eng();        
+        e1.read();       
+        e1.display();
+        d1.readbonus();
+        System.out.println("Total salary is: "+d1.calculate());
+    }
+}
